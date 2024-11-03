2013a938e8fc07932aeab152427c35f3f932eacc
ansnekit <ansnekit@gmail.com>
2024-11-03 17:46:05 +0300

    sprint_2 add components from, header

---
+++ src/components/Header/index.ts
@@ -0,0 +1,10 @@
+import { BaseComponent } from '@/services/base-component';
+import HeaderTemplate from './header.hbs?raw';
+import './header.pcss';
+
+
+export default class Header extends BaseComponent {
+    render() {
+        return this.compile(HeaderTemplate);
+    }
+} 
