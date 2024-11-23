8c611dfea900a3dd24792f288dfaedc22a6ce682
ansnekit <ansnekit@gmail.com>
2024-11-23 19:16:49 +0300

    sprint_2 add http transport

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import Input from "@/components/Input";
+import Input from '@/components/Input';
 
 export const uppercase = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
 
@@ -15,7 +15,19 @@ export const validate = (rules: Record<string, string | true>) => {
     return Object.values(rules).every((validate) => validate === true);
 };
 
-export const setInputValidationState = (input: Input, value: string, errorText: string) => {    
-    return input.setProps({value, error: errorText});
+export const setInputValidationState = (input: Input, value: string, errorText: string) => {
+    return input.setProps({ value, error: errorText });
 };
 
+export const queryStringify = (data: unknown) => {
+    if (!data || typeof data !== 'object') {
+        throw new Error('Data must be object');
+    }
+
+    const keys = Object.keys(data);
+    return keys.reduce((result, key, index) => {
+        const param = (data as Record<string, string>)[key];
+        const appendSymbol = `${index < keys.length - 1 ? '&' : ''}`;
+        return `${result}${key}=${param}${appendSymbol}`;
+    }, '?');
+};
