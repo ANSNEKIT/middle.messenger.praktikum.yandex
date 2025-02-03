e7fd7f4529630bd51a6b64a556956ece25fcc61a
ansnekit <ansnekit@gmail.com>
2025-02-03 22:10:33 +0300

    sprint_3 fix button component

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,8 +1,20 @@
 import { Block } from '@/services/base-component';
 import './button.pcss';
 import buttonTeplate from './button.hbs?raw';
+import { IProps } from '@/types';
+
+interface IButtonProps extends IProps {
+    id?: string;
+    class?: string;
+    disabled?: boolean;
+    type?: 'button' | 'submit';
+    text?: Block | string;
+}
 
 export default class Button extends Block {
+    constructor(_: string, props: IButtonProps) {
+        super('button', { ...props, type: props?.type ?? 'button' });
+    }
     render() {
         return this.compile(buttonTeplate);
     }
